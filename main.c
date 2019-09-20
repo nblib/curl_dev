675be66a7ee06635827dedbae190a8fb7c3e8029
hewe <tingshage@163.com>
2019-09-20 02:14:22 +0000

    add command argument

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include "request.h"
 #include<stdlib.h>
 #include<time.h>
-
+#include<string.h>
 
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
@@ -11,13 +11,59 @@ int global_Success;
 
 
 
-int THREAD_COUNT = 20; // 启用线程数
-int TOTAL_COUNT = 100;  // 总请求数量
+long THREAD_COUNT = 1; // 启用线程数
+long TOTAL_COUNT = 1;  // 总请求数量
 char *URL = "https://example.com"; // url
-int DEBUG = 1; //1输出响应内容,0不输出
+int DEBUG = 0; //1输出响应内容,0不输出
 
+
+char *useage = "curl_dev [debug] [totalcount] [threadcount] url\n比如: curl_dev https://www.example.com  将会默认请求一次使用一个线程\n curl_dev 20 https://www.example.com  将会请求20次使用一个线程\n curl_dev 20 10 https://www.example.com 总共请求20次使用10个线程\n curl_dev debug 20 10 https://www.example.com 开启调试模式,输出响应内容\n";
+
+int arg_parse(int argc, char* argv[])
+{
+  if (argc <= 1)
+  {
+    printf("require url\n");
+    printf(useage);
+    return -1;    
+  }
+  int i;
+  for(i = 1; i < argc; i++)
+  {
+    if (strcmp(argv[i], "debug") == 0)
+    {
+      DEBUG = 1;
+    }
+    else if(('0' <= argv[i][0]) && ('9' >= argv[i][0]))
+    {
+      long count = strtol(argv[i], NULL, 10);
+      if (count > 0)
+      {
+          if(TOTAL_COUNT <= 1)
+          {
+            TOTAL_COUNT = count;
+          }
+          else
+          {
+              THREAD_COUNT = count;
+          }
+      }
+    }
+    else
+    {
+      URL = argv[i];
+    }
+  }
+  return 0;
+}
 int main(int argc, char *argv[])
 {
+  //参数处理
+  if(arg_parse(argc, argv) != 0)
+  {
+    return 1;
+  }
+
   //statistic init
   pthread_mutex_init(&mutex, NULL);
   //libcurl init
